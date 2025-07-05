4417f310e9163c23f5f5af64d57f84fb391345c7
otitolaiyejoshua <72415298+otitolaiyejoshua@users.noreply.github.com>
2025-07-04 23:05:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.use(cors({
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
